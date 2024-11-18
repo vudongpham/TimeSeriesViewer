101472a138ebf3de8038f2c61233499f91ff5f69
vudongpham <vudong.pham27@gmail.com>
2024-11-18 14:51:26 +0100

    Update basemap

--- map.js
+++ map.js
@@ -62,14 +62,12 @@ for (let index = 0; index < doy_list_s2.length; index++) {
 
 var map = L.map('map').setView([52.48044, 13.23972], 11);
 
-var Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
-    minZoom: 0,
-    maxZoom: 20,
-    attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'jpg'
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 19,
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
-Stadia_AlidadeSatellite.addTo(map);
+Esri_WorldImagery.addTo(map);
 
 const polygon_coordinates = [
     [
